6514361e84a7d4bb4e29bdfaa420c1b07e8ed704
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-31 09:44:45 -0700

    .map through creating function complete

--- map.js
+++ map.js
@@ -2,16 +2,25 @@
 // OUTPUT: an array of the names
 // REQS: use .map
 
-export const getCountryNames = (countries) => {};
+export const getCountryNames = (countries) => {
+  const countryNames = countries.map((country) => country.name);
+  return countryNames;
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: an array of the names and their iso2 country codes using .map
 // REQS: use .map
 
-export const getCountryCodes = (countries) => {};
+export const getCountryCodes = (countries) => {
+  const nameAndCode = countries.map((country) => [country.name, country.iso2]);
+  return nameAndCode;
+};
 
 // INPUT: the array of shoes from data.js
 // OUTPUT: an array of the brands
 // REQS: use .map
 
-export const getBrands = (brands) => {};
+export const getBrands = (brands) => {
+  const shoeBrands = brands.map((brand) => brand.brand);
+  return shoeBrands;
+};
