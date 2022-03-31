b5f6c39e474fe7714e35da9ea3ab64cfee9be5ca
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-31 11:56:30 -0700

    completed writing out reduce functions

--- filter.js
+++ filter.js
@@ -2,22 +2,28 @@
 // OUTPUT: only countries in Oceana
 // REQS: use .filter
 
-export const filterOceania = (countries) => {};
+export const filterOceania = (countries) => {
+  return countries.filter((country) => country.continent === 'Oceania');
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: only countries whose name that include the letters 'au'
 // REQS: use .filter
 
 export const filterAu = (countries) => {
-  return countries.filter((country) => country.name.includes('au'));
+  return countries.filter((country) => country.name.toLowerCase().includes('au'));
 };
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: only dogs that are 2 years old or younger
 // REQS: use .filter
-export const filterYoungDogs = (dogs) => {};
+export const filterYoungDogs = (dogs) => {
+  return dogs.filter((dog) => dog.age < 3);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: a list of names of dogs that are 2 years old or younger
 // REQS: use .filter AND .map OR use your filterYoungDogs function and a map
-export const getYoungDogNames = (dogs) => {};
+export const getYoungDogNames = (dogs) => {
+  return dogs.filter((dog) => dog.age < 3).map((dog) => dog.name);
+};
--- reduce.js
+++ reduce.js
@@ -2,9 +2,16 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
-
+export const getSneaks = (brands) => {
+  return brands.reduce((acc, brand) => [...acc, ...brand.shoes], []);
+};
+// we're making new arrays because we have an putside []
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+  const sum = dogs.reduce((acc, dog) => (acc += dog.age), 0);
+  return sum / dogs.length;
+};
+
+// dogs.reduce((acc, dog) = > acc += dog.age), 0 /dogs.length
